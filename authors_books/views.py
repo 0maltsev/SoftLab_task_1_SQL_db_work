a30ad596121d4f41b3b0356d37bc2777f0566be8
J1zz <maltsev.oiu@phystech.edu>
2021-12-15 19:10:04 +0300

    new loggers format + 500 status

--- authors_books/views.py
+++ authors_books/views.py
@@ -27,62 +27,55 @@ def main(request):
         for row in rows:
             counters.append(row[0])
         logger.info('client requests main screen')
-    except:
+    except Exception as ex:
         problem_check = 1
-        logger.error('cant get info from db on main screen')
+        logger.error(ex, exc_info=True)
         s.rollback()
+        return HttpResponse(status=500, content="Internal Server Error")
     finally:
         s.close()
 
-    if problem_check == 0:
-        context = {'authors': authors, 'counters': counters}
-        return render(request, 'main.html', context)
-    else:
-        return HttpResponse("Something went wrong..")
+    context = {'authors': authors, 'counters': counters}
+    return render(request, 'main.html', context)
+
 
 
 def book_list(request, pk):
 
     session = sessionmaker(bind=engine)
     s = session()
-    problem_check = 0
     try:
         titles = s.query(Book).filter(Book.author_id == pk).all()
         authors = s.query(Author).filter(Author.id_author == pk).all()
         logger.info('client requests title_list')
-    except:
-        problem_check = 1
+    except Exception as ex:
+        logger.error(ex, exc_info=True)
         s.rollback()
-        logger.error('cant get info from db on title_list screen')
+        return HttpResponse(status=500, content="Internal Server Error")
     finally:
         s.close()
 
     form = NewBookForm()
     form.fields['author_id'].initial = pk
     if request.method == 'POST':
-        problem_check = 0
         form = NewBookForm(request.POST)
         if form.is_valid():
             try:
                 author_id = form.cleaned_data['author_id']
                 title = form.cleaned_data['title']
-                print(author_id, title)
+
                 s.add_all([Book(author_id=author_id, title=title)])
                 s.commit()
                 logger.info('client adds new book to title_list')
-            except:
-                problem_check = 1
+            except Exception as ex:
+                logger.error(ex, exc_info=True)
                 s.rollback()
-                logger.error('cant add book on title_list screen')
+                return HttpResponse(status=500, content="Internal Server Error")
             finally:
                 s.close()
-            if problem_check == 0:
-                return redirect(request.META['HTTP_REFERER'])
-            else:
-                return HttpResponse("Something went wrong..")
 
-    if problem_check == 0:
-        context = {'titles': titles, 'authors': authors, 'form': form}
-        return render(request, 'book_list.html', context)
-    else:
-        return HttpResponse("Something went wrong..")
+            return redirect(request.META['HTTP_REFERER'])
+
+    context = {'titles': titles, 'authors': authors, 'form': form}
+    return render(request, 'book_list.html', context)
+
